d021d1b0a2eb9108ef2f7ee1148c05568073ddf1
Nahombadebo47 <nahombadebo47@gmail.com>
2024-08-30 14:03:01 +0300

    Recipe App Initial Touches, Recipe list and add recipe component

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+// App.js
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  );
+}
+
+export default App;
