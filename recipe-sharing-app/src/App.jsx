e5688cd1a66a3632683ff9a6492c197a582a51d8
Nahombadebo47 <nahombadebo47@gmail.com>
2024-08-30 14:51:37 +0300

    new components function edit and delete functionalities thoroughly to ensure state updates are handled properly across all components.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,23 @@
-// App.js
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+// App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './RecipeList';
+import AddRecipeForm from './AddRecipeForm';
+import RecipeDetails from './RecipeDetails';
 
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <RecipeList />
-      <AddRecipeForm />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe App</h1>
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
