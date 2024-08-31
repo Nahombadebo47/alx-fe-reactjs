f0d4debb70fcf38c92fdaacc6a8be307f8f359a7
Nahombadebo47 <nahombadebo47@gmail.com>
2024-08-31 14:31:31 +0300

    Added Functionalities in recipe store for update and delete, Three new Components for delete button and edit button, and also a recipe details component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,22 @@
 // App.jsx
 import './App.css';
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm'; // Corrected import
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <RecipeList />
-      <AddRecipeForm />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+        <AddRecipeForm />
+      </div>
+    </Router>
   );
 }
 
