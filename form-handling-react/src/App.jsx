99496b0644d36b8233357fa7e8d02d8acac27016
Nahombadebo47 <nahombadebo47@gmail.com>
2024-09-03 15:01:55 +0300

    Minor

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 
-import FormikForm from "./components/FormikForm"
+import FormikForm from "./components/formikForm"
 import RegistrationForm from "./components/RegistrationForm"
 
 function App() {
