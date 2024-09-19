54477f228a2e7178d33816618bebe81fede489f3
Nahombadebo47 <nahombadebo47@gmail.com>
2024-09-19 19:52:28 +0300

    tailwind

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Purging unused styles
+  darkMode: false, // Disable dark mode (you can set to 'media' or 'class' to enable)
   theme: {
     extend: {},
+  },
+  variants: {
+    extend: {}, // Extend variants here if needed
   },
   plugins: [],
 };
