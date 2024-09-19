45a7a39069554701cbd32e088b3685258182fb45
Nahombadebo47 <nahombadebo47@gmail.com>
2024-09-19 20:39:22 +0300

    search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const fetchUserData = async (username, location = '', minRepos = 0) => {
-  // Construct the search query with optional location and minimum repositories
-  let query = `q=${username}`;
+  // Construct the search query
+  let query = `${username}`;
   
   if (location) {
     query += `+location:${location}`;
@@ -12,10 +12,11 @@ export const fetchUserData = async (username, location = '', minRepos = 0) => {
     query += `+repos:>=${minRepos}`;
   }
 
-  // Correct GitHub Search API URL
-  const API_URL = `https://api.github.com/search/users?${query}`;
+  // GitHub Search API URL
+  const API_URL = `https://api.github.com/search/users?q=${query}`;
 
   try {
+    // Make the GET request using the constructed API URL
     const response = await axios.get(API_URL);
     return response.data.items;  // Return the list of users
   } catch (error) {
