3e7d832ad7b5e776ab272bff1425b3a32a604525
Nahombadebo47 <nahombadebo47@gmail.com>
2024-09-19 19:56:14 +0300

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="w-36 h-36 rounded-full mx-auto"
+          className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-base">
+        <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500">
+          John Doe
+        </h1>
+        <p className="text-sm sm:text-base text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
